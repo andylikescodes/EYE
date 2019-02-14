4d493ca7fd9bfca821b6839cc3d3ecb015472729
Dehua Liang <andylikescodes@gmail.com>
2019-02-13 17:04:21 -0800

    First commit: Creating necessary files for the experiment.

---
+++ constants.py
@@ -0,0 +1,8 @@
+# Some experiment constans
+
+DISPSIZE = (1920, 1080)
+
+FGC = (-1, -1, -1)
+BGC = (0, 0, 0)
+
+
---
+++ experiment.py
@@ -0,0 +1,6 @@
+from psychopy.visual import Window
+from constants import *
+
+disp = Window(size=DISPSIZE, units='pix', fullscr=True)
+disp.close()
+
