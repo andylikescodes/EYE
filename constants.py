c6344397b8f9395f95d794c77dfd1fe204b9b307
Arman Mansourian <mansourianarman@gmail.com>
2019-02-23 12:47:54 -0800

    COLORS dict added for variability

--- constants.py
+++ constants.py
@@ -1,26 +1,41 @@
 import os
 
+# DUMMYMODE should be True if no tracker is attached
+# DUMMYMODE = False
+DUMMYMODE = True
+
+# Get Participant ID for logfile
+LOGFILENAME = raw_input("Participant ID: ")
+LOGFILE = LOGFILENAME[:]
+
 # The DISPTYPE can be either 'pygame' or 'psychopy'
-DISPTYPE = 'psychopy'
-# DISPTYPE = 'pygame'
+# DISPTYPE = 'psychopy'
+DISPTYPE = "pygame"
 
 # The DISPSIZE should match your monitor's resolution
 # DISPSIZE = (1920, 1080)
 DISPSIZE = (1440, 900)
 
-# DUMMYMODE should be True if no tracker is attached
-# DUMMYMODE = False
-DUMMYMODE = True
-
 # Set the TRACKERTYPE to the brand you use
-# TRACKERTYPE = 'eyelink'
-TRACKERTYPE = 'dummy'
+TRACKERTYPE = "eyelink"
+SACCVELTHRESH = 35
+SACCACCTHRESH = 9500
 
-# Foreground colour set to green
-FGC = (0, 128, 0)
+# Dictionary of color codes
+COLORS = {"darkgreen": (0, 100, 0),
+          "green": (0, 128, 0),
+          "lightgreen": (0, 204, 0),
+          "lime": (0, 255, 0),
+          "lightgray": (211, 211, 211),
+          "silver": (192, 192, 192),
+          "gray": (169, 169, 169),
+          "darkgray": (128, 128, 128)}
 
-# Background colour set to gray
-BGC = (128, 128, 128)
+# Foreground colour set to lime
+FGC = COLORS["lime"]
+
+# Background colour set to darkgray
+BGC = COLORS["darkgray"]
 
 # Fixation mark time (milliseconds)
 FIXTIME = 5000
