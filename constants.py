87b10079ed555f3caf166c1f0d2d965dd12bb496
Arman Mansourian <mansourianarman@gmail.com>
2019-02-23 11:15:28 -0800

    updated constants

--- constants.py
+++ constants.py
@@ -1,8 +1,31 @@
-# Some experiment constans
+import os
 
-DISPSIZE = (1920, 1080)
+# The DISPTYPE can be either 'pygame' or 'psychopy'
+DISPTYPE = 'psychopy'
+# DISPTYPE = 'pygame'
 
-FGC = (-1, -1, -1)
-BGC = (0, 0, 0)
+# The DISPSIZE should match your monitor's resolution
+# DISPSIZE = (1920, 1080)
+DISPSIZE = (1440, 900)
 
+# DUMMYMODE should be True if no tracker is attached
+DUMMYMODE = True
 
+# Set the TRACKERTYPE to the brand you use
+# TRACKERTYPE = 'eyelink'
+TRACKERTYPE = 'dummy'
+
+# Foreground colour set to green
+FGC = (0, 128, 0)
+
+# Background colour set to gray
+BGC = (128, 128, 128)
+
+# Fixation mark time (milliseconds)
+FIXTIME = 5000
+
+# Image time (milliseconds)
+IMGTIME = 500
+
+# Get the path to the current folder
+DIR = os.path.dirname(__file__)
