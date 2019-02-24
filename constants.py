d17582d8470607f7ef2ee6b9d767e96006d4061b
Dehua Liang <andylikescodes@gmail.com>
2019-02-24 11:02:40 -0800

    leftover code

--- constants.py
+++ constants.py
@@ -1,8 +1,14 @@
 # Some experiment constans
 
-DISPSIZE = (1920, 1080)
 
-FGC = (-1, -1, -1)
-BGC = (0, 0, 0)
+# DISPSIZE = (1920, 1080)
+#
+# FGC = (-1, -1, -1)
+# BGC = (0, 0, 0)
 
 
+DISPTYPE = 'pygame'
+DISPSIZE = (1280, 800)
+
+FGC = (0, 0, 0)
+BGC = (128, 128, 128)
--- experiment.py
+++ experiment.py
@@ -1,6 +1,30 @@
-from psychopy.visual import Window
+# from psychopy.visual import Window
+# from constants import *
+#
+# disp = Window(size=DISPSIZE, units='pix', fullscr=True)
+# disp.close()
+
 from constants import *
+from pygaze.display import Display
+import pygaze.libtime as timer
+from pygaze.screen import Screen
 
-disp = Window(size=DISPSIZE, units='pix', fullscr=True)
+disp = Display()
+# fixscreen = Screen()
+# fixscreen.draw_fixation(fixtype='dot')
+#
+imgscreen = Screen()
+imgscreen.draw_image('test.jpg')
+
+# disp.fill(fixscreen)
+# disp.show()
+# timer.pause(2000)
+
+disp.fill(imgscreen)
+timer.pause(200000)
+disp.show()
+timer.pause(200000)
+
+timer.pause(200000)
+#
 disp.close()
-
