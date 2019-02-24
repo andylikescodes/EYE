2c0c303e383b0af488465f9cb34742c2931ce7be
Dehua Liang <andylikescodes@gmail.com>
2019-02-24 11:05:22 -0800

    resolve conflicts

--- constants.py
+++ constants.py
@@ -1,14 +1,57 @@
-# Some experiment constans
+import os
 
+# Get the path to the current folder
+DIR = os.path.dirname(os.path.abspath(__file__))
 
-# DISPSIZE = (1920, 1080)
-#
-# FGC = (-1, -1, -1)
-# BGC = (0, 0, 0)
+# DUMMYMODE should be True if no tracker is attached
+DUMMYMODE = True
 
+# Get Participant ID for logfile
+LOGFILENAME = raw_input("Participant ID: ")
+LOGFILE = LOGFILENAME[:]
 
-DISPTYPE = 'pygame'
-DISPSIZE = (1280, 800)
+# The DISPTYPE can be either 'pygame' or 'psychopy'
+DISPTYPE = "psychopy"
 
-FGC = (0, 0, 0)
-BGC = (128, 128, 128)
+# The DISPSIZE should match your monitor's resolution
+DISPSIZE = (1440, 900)
+
+# Set the TRACKERTYPE to the brand you use
+TRACKERTYPE = "eyelink"
+SACCVELTHRESH = 35
+SACCACCTHRESH = 9500
+
+# Dictionary of color codes
+COLORS = {"darkgreen": (0, 100, 0),
+          "green": (0, 128, 0),
+          "lightgreen": (0, 204, 0),
+          "lime": (0, 255, 0),
+          "lightgray": (211, 211, 211),
+          "silver": (192, 192, 192),
+          "darkgray": (169, 169, 169),
+          "gray": (128, 128, 128)}
+
+# Foreground colour set to lime
+FGC = COLORS["green"]
+
+# Background colour set to darkgray
+BGC = COLORS["darkgray"]
+
+# Fixation mark time (milliseconds)
+FIXTIME = 5000
+
+# Image time (milliseconds)
+IMGTIME = 500
+
+# Number of trials
+while True:
+    try:
+        NUMTRIALS = int(raw_input("Number of trials: "))
+    except ValueError:
+        print "Invalid input: non-integer value"
+        continue
+    if NUMTRIALS < 1 or NUMTRIALS > 100:
+        print "Invalid input: out of range, select from 1 to 100 trials"
+        continue
+    else:
+        break
--- experiment.py
+++ experiment.py
@@ -1,30 +1,89 @@
-# from psychopy.visual import Window
-# from constants import *
-#
-# disp = Window(size=DISPSIZE, units='pix', fullscr=True)
-# disp.close()
-
+import string
 from constants import *
-from pygaze.display import Display
+from pygaze.libscreen import Display, Screen
+from pygaze.libinput import Keyboard
+from pygaze.eyetracker import EyeTracker
 import pygaze.libtime as timer
-from pygaze.screen import Screen
 
+# Initialize Display to interact with monitor
 disp = Display()
-# fixscreen = Screen()
-# fixscreen.draw_fixation(fixtype='dot')
-#
-imgscreen = Screen()
-imgscreen.draw_image('test.jpg')
 
-# disp.fill(fixscreen)
-# disp.show()
-# timer.pause(2000)
+# Initialize Keyboard to collect key presses
+kb = Keyboard(keylist=None, timeout=None)
 
-disp.fill(imgscreen)
-timer.pause(200000)
+# Create Screen for task instructions
+inscr = Screen()
+inscr.draw_text(text="Instructions:\n" +
+                     "Press spacebar when you see your letter.\n\n" +
+                     "(Press any key to begin)",
+                fontsize=24)
+
+# Create Screen with central fixation cross
+fixscr = Screen()
+fixscr.draw_fixation(fixtype="cross", diameter=16)
+
+# Create Trial Screen and Screen to draw alphabetic letters on later
+trialscr = Screen()
+imgscr = Screen()
+
+# Initialise new EyeTracker
+tracker = EyeTracker(disp)
+tracker.calibrate()
+
+# Show instructions on Display then wait for key press
+disp.fill(inscr)
 disp.show()
-timer.pause(200000)
+kb.get_key()
 
-timer.pause(200000)
-#
+# Iterate through n trials
+for n in range(1, NUMTRIALS + 1):
+
+    # Show trial number
+    trialscr.clear()
+    trialscr.draw_text(text="Trial No.%s\n(Press any key to begin)" % n,
+                       fontsize=24)
+    disp.fill(trialscr)
+    disp.show()
+    kb.get_key()
+
+    # Start recording and display status message on EyeLink trackers
+    tracker.start_recording()
+    tracker.status_msg("Trial No.%s" % n)
+    tracker.log("TRIAL %s START" % n)
+
+    # Show fixation Screen on Display
+    disp.fill(fixscr)
+    disp.show()
+    tracker.log("FIXATION ONSET")
+    timer.pause(FIXTIME)
+
+    # Show alphabet Screen on Display
+    for letter in string.ascii_lowercase:
+
+        # Display letter
+        imgscr.clear()
+        imgscr.draw_text(text=letter, fontsize=64)
+        disp.fill(imgscr)
+        disp.show()
+        tracker.log("IMAGE ONSET, letter=%s" % letter)
+        timer.pause(IMGTIME)
+
+        # Clear Display
+        tracker.log("IMAGE OFFSET, letter=%s" % letter)
+        disp.fill()
+        disp.show()
+
+    # Log the end of trial
+    tracker.log("TRIAL %s END" % n)
+    tracker.stop_recording()
+
+# Notify end to participant then wait for key press
+inscr.clear()
+inscr.draw_text(text="All done!\n\n(Press any key)", fontsize=24)
+disp.fill(inscr)
+disp.show()
+kb.get_key()
+
+# Close connection to eye tracker and Display
+tracker.close()
 disp.close()
