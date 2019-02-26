49efba4ec9fe8c0e791b0e680b6607f7939813a4
Arman Mansourian <mansourianarman@gmail.com>
2019-02-25 16:54:36 -0800

    button press working, calculates delta_t between display and reaction

--- experiment.py
+++ experiment.py
@@ -1,9 +1,9 @@
 import string
-from constants import *
-from pygaze.libscreen import Display, Screen
-from pygaze.libinput import Keyboard
+from pygaze import libtime
 from pygaze.eyetracker import EyeTracker
-import pygaze.libtime as timer
+from pygaze.libinput import Keyboard
+from pygaze.libscreen import Display, Screen
+from constants import *
 
 # Initialize Display to interact with monitor
 disp = Display()
@@ -13,14 +13,14 @@ kb = Keyboard(keylist=None, timeout=None)
 
 # Create Screen for task instructions
 inscr = Screen()
-inscr.draw_text(text="Instructions:\n" +
-                     "Press spacebar when you see your letter.\n\n" +
-                     "(Press any key to begin)",
-                fontsize=24)
+inscr.draw_text(
+            text='Instructions:\nPress ENTER when you see your letter.\n\n' +
+                 '(Press any key to continue)',
+            fontsize=24)
 
 # Create Screen with central fixation cross
 fixscr = Screen()
-fixscr.draw_fixation(fixtype="cross", diameter=16)
+fixscr.draw_fixation(fixtype='cross', diameter=16)
 
 # Create Trial Screen and Screen to draw alphabetic letters on later
 trialscr = Screen()
@@ -40,46 +40,69 @@ for n in range(1, NUMTRIALS + 1):
 
     # Show trial number
     trialscr.clear()
-    trialscr.draw_text(text="Trial No.%s\n(Press any key to begin)" % n,
-                       fontsize=24)
+    trialscr.draw_text(
+                    text='Trial #%s\n(Press any key to begin)' % n,
+                    fontsize=24)
     disp.fill(trialscr)
     disp.show()
     kb.get_key()
 
     # Start recording and display status message on EyeLink trackers
     tracker.start_recording()
-    tracker.status_msg("Trial No.%s" % n)
-    tracker.log("TRIAL %s START" % n)
+    tracker.status_msg('Trial No.%s' % n)
+    tracker.log('TRIAL %s START' % n)
 
     # Show fixation Screen on Display
     disp.fill(fixscr)
     disp.show()
-    tracker.log("FIXATION ONSET")
-    timer.pause(FIXTIME)
+    tracker.log('FIXATION ONSET')
+    libtime.pause(FIXTIME)
+
+    # Bool for looping
+    pressed = False
 
     # Show alphabet Screen on Display
     for letter in string.ascii_lowercase:
 
-        # Display letter
+        # Check if pressed
+        if pressed:
+            break
+
+        # Prepare image Screen
         imgscr.clear()
+        imgscr.draw_text(text=letter, fontsize=64)
+        tracker.log('IMAGE ONSET, letter=%s' % letter)
+
+        # Display letter
         imgscr.draw_text(text=letter, fontsize=64)
         disp.fill(imgscr)
         disp.show()
-        tracker.log("IMAGE ONSET, letter=%s" % letter)
-        timer.pause(IMGTIME)
+
+        # Initialize timer for each letter
+        t0 = libtime.get_time()
+        tstim = libtime.get_time()
+
+        # Check for input
+        key, presstime = kb.get_key(
+                                keylist=['return', 'escape'],
+                                timeout=IMGTIME)
+        if key:
+            tstim = presstime - t0
+            tracker.log('ACTION RECORDED, delta_t=%.2f ms' % tstim)
+            pressed = True
 
         # Clear Display
-        tracker.log("IMAGE OFFSET, letter=%s" % letter)
+        tracker.log('IMAGE OFFSET, letter=%s' % letter)
         disp.fill()
         disp.show()
 
     # Log the end of trial
-    tracker.log("TRIAL %s END" % n)
+    tracker.log('TRIAL %s END' % n)
     tracker.stop_recording()
 
 # Notify end to participant then wait for key press
 inscr.clear()
-inscr.draw_text(text="All done!\n\n(Press any key)", fontsize=24)
+inscr.draw_text(text='All done!\n\n(Press any key to exit)', fontsize=24)
 disp.fill(inscr)
 disp.show()
 kb.get_key()
